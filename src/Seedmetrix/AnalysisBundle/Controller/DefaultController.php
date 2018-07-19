46590c7004c13fca04de9a1397d926706b748c71
YAHIA HELLALI Amine <amine.yahia-hellali@onisep.fr>
2018-07-19 08:18:27 +0200

    test add ajax

--- src/Seedmetrix/AnalysisBundle/Controller/DefaultController.php
+++ src/Seedmetrix/AnalysisBundle/Controller/DefaultController.php
@@ -3,30 +3,109 @@
 namespace Seedmetrix\AnalysisBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
+    const ACTION_TYPE_READ="01";
+
     public function indexAction()
     {
-        //
-        
-       // return $this->render('@Analysis/Default/index.html.twig');
-       //$histo = [];
-        // $chart = $this->get('app.chart');
-        //$histo  = ['val1' => 2 , 'val2' => 4, 'val3' => 8, 'va14' => 5 ] ;
-        //var_dump($histo);
-       // die();
-        // return $this->render('@Analysis/Default/index.html.twig' , array('histo' => $histo));
-      
-        $existFile = $this->container->get('filexist');
-        // Je pars du principe que $text contient le texte d'un message quelconque
-        
-        $file = 'C:\wamp64\www\input\exemple.xlsx';
-      //  if (!isset($file)) {
-      if($existFile->FileExist($file));
-        //throw new \Exception('Votre message a été détecté comme spam !');
-          return $this->render('@Analysis/Default/error.html.twig');
-      //  }
-     //  return $this->render('@Analysis/Default/index.html.twig');
+        return $this->render('@Analysis/Default/index.html.twig');
     }
+
+    public function findCollegeAction(Request $request){
+
+        $results = "";
+       // if($request->isXmlHttpRequest()) {
+            $collegestr = $request->request->get("collegestr");
+            if (!empty($collegestr)) {
+                $url = "http://rcu.onisep.fr:8080/rcuWS/service/client/etablissements/";
+                $ch = curl_init($url);
+                $params = array(
+                    "recherche" => $collegestr ,
+                    "traitementApplication" => "Ecran saisie commandes",
+                    "typeAction" => "01",
+                    "codeApplication" => "01"
+                );
+                // encodage en json de notre objet
+                $content = json_encode($params, JSON_FORCE_OBJECT);
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $content);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                        'Content-Type: application/json',
+                        'Content-Length: ' . strlen($content))
+                );
+                curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+                curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+
+                //execute post
+                $results = curl_exec($ch);
+                //var_dump($results);
+                //close connection
+                curl_close($ch);
+            }
+      //  }
+
+        return $this->render('@Analysis/Default/index.html.twig', array(
+            'villes' => $results
+        ));
+    }
+
+    public function rechercheAction(Request $request)
+    {
+        $colleges = array();
+        $collegestr = $request->request->get("mot_cle");
+        $url = "http://rcu.onisep.fr:8080/rcuWS/service/client/etablissements/";
+        $ch = curl_init($url);
+        $params = array(
+            "recherche" => $collegestr,
+            "traitementApplication" => "Ecran saisie commandes",
+            "typeAction" => "01",
+            "codeApplication" => "01"
+        );
+        if (!empty($collegestr)) {
+            $content = json_encode($params, JSON_FORCE_OBJECT);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $content);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                'Content-Type: application/json',
+                    'Content-Length: ' . strlen($content))
+            );
+            curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+            $results = curl_exec($ch);
+            if (!$results) {
+               // echo curl_error($ch); ( to do ecrire la function error($ch) //
+            } else {
+                $data = json_decode($results);
+                $nb = 0 ;
+                while ($nb < 10) {
+                    foreach ($data as $key => $value) {
+                        //$colleges [] = $value;
+                        //$nb += 1;
+                        foreach ($value as $val) {
+                            if($nb < 10) {
+                               // echo "<br>" . $nb . "<br>";
+                                $nb = $nb + 1;
+                                $colleges[] = $val;
+                            }
+                        }
+                    }
+                }
+            }
+            //var_dump($colleges) ;
+            //close connection
+            curl_close($ch);
+            $villes = $colleges;
+
+
+        }
+        return $this->render('@Analysis/Default/error.html.twig', array(
+            'villes' => $colleges
+        ));
+    }
+
 }
